6811ef6533cbb6e0d21b4ffc0dd611e6fe20eec5
J-Budd <168031724+J-Budd@users.noreply.github.com>
2024-12-15 11:22:37 -0500

    Updated checks for schema.sql

--- schema.sql
+++ schema.sql
@@ -1,43 +1,51 @@
-DROP DATABASE IF EXISTS dolphin_crm;
-CREATE DATABASE dolphin_crm;
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'dolphin_crm')
+    BEGIN
+        CREATE DATABASE dolphin_crm;
+    END;
 USE dolphin_crm;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users`(
-    `id` INTEGER(12) NOT NULL auto_increment,
-    `firstname` VARCHAR(32) DEFAULT NULL,
-    `lastname` VARCHAR(32) DEFAULT NULL,
-    `password` VARCHAR(20) DEFAULT NULL,
-    `email` VARCHAR(32) DEFAULT NULL,
-    `role` enum('Member','Admin') NOT NULL DEFAULT 'Member',
-    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-);
+IF object_id('users', 'U') is null
+    BEGIN
+        CREATE TABLE `users`(
+            `id` INTEGER(12) NOT NULL auto_increment,
+            `firstname` VARCHAR(32) DEFAULT NULL,
+            `lastname` VARCHAR(32) DEFAULT NULL,
+            `password` VARCHAR(20) DEFAULT NULL,
+            `email` VARCHAR(32) DEFAULT NULL,
+            `role` enum('Member','Admin') NOT NULL DEFAULT 'Member',
+            `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`)
+        );
+    END;
 
 
-DROP TABLE IF EXISTS `contacts`;
-CREATE TABLE `contacts`(
-    `id` INTEGER(12) NOT NULL auto_increment,
-    `title` VARCHAR(32) DEFAULT NULL,
-    `firstname` VARCHAR(32) DEFAULT NULL,
-    `lastname` VARCHAR(32) DEFAULT NULL,
-    `email` VARCHAR(32) DEFAULT NULL,
-    `telephone` VARCHAR(16) DEFAULT NULL,
-    `company` VARCHAR(64) DEFAULT NULL,
-    `type` enum('Sales Lead','Support') NOT NULL DEFAULT 'Sales Lead',
-    `assigned_to` INTEGER(12) NOT NULL,
-    `created_by` INTEGER(12) NOT NULL,
-    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-);
+IF object_id('contacts', 'U') is null
+    BEGIN
+        CREATE TABLE `contacts`(
+            `id` INTEGER(12) NOT NULL auto_increment,
+            `title` VARCHAR(32) DEFAULT NULL,
+            `firstname` VARCHAR(32) DEFAULT NULL,
+            `lastname` VARCHAR(32) DEFAULT NULL,
+            `email` VARCHAR(32) DEFAULT NULL,
+            `telephone` VARCHAR(16) DEFAULT NULL,
+            `company` VARCHAR(64) DEFAULT NULL,
+            `type` enum('Sales Lead','Support') NOT NULL DEFAULT 'Sales Lead',
+            `assigned_to` INTEGER(12) NOT NULL,
+            `created_by` INTEGER(12) NOT NULL,
+            `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+            `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`)
+        );
+    END;
 
-DROP TABLE IF EXISTS `notes`;
-CREATE TABLE `notes`(
-    `id` INTEGER(12) NOT NULL auto_increment,
-    `contact_id` INTEGER(12) NOT NULL,
-    `comment` TEXT(65535) DEFAULT NULL,
-    `created_by` INTEGER(12) NOT NULL,
-    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-);
+IF object_id('notes', 'U');
+    BEGIN
+        CREATE TABLE `notes`(
+            `id` INTEGER(12) NOT NULL auto_increment,
+            `contact_id` INTEGER(12) NOT NULL,
+            `comment` TEXT(65535) DEFAULT NULL,
+            `created_by` INTEGER(12) NOT NULL,
+            `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`)
+        );
+    END;
